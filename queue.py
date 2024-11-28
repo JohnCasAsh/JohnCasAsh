7014b477637b91de1de464539251d7cecc9df16a
John Asley C. Lunnay <148631754+JohnCasAsh@users.noreply.github.com>
2024-11-28 20:57:46 +0800

    Update queue.py

--- queue.py
+++ queue.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Serve the HTML file
+    message = "Hello, World!"  # Message to display in the HTML
+    return render_template('index.html', message=message)  # Pass message to HTML
 
 if __name__ == '__main__':
     app.run(debug=True)
-
